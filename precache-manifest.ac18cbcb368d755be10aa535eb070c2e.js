d07c5410f8bfc0eefad1e902dac1c86a6fbd16bb
Harim Sanchez <harimsanchez@gmail.com>
2020-02-23 13:49:56 -0800

    Updates

---
+++ precache-manifest.ac18cbcb368d755be10aa535eb070c2e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0929a8c6202e4ffa4422c5d7858bba04",
+    "url": "/fresh-avocado/index.html"
+  },
+  {
+    "revision": "370da9692d55b6a32b64",
+    "url": "/fresh-avocado/static/css/main.e9c518f6.chunk.css"
+  },
+  {
+    "revision": "b5abcc3335dc390cf92b",
+    "url": "/fresh-avocado/static/js/2.fea08649.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fresh-avocado/static/js/2.fea08649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370da9692d55b6a32b64",
+    "url": "/fresh-avocado/static/js/main.e438da6a.chunk.js"
+  },
+  {
+    "revision": "b6a764af05a20518d39a",
+    "url": "/fresh-avocado/static/js/runtime-main.45d9a014.js"
+  },
+  {
+    "revision": "7b2e77f3b3831497100838ab8e3bc343",
+    "url": "/fresh-avocado/static/media/daniel.7b2e77f3.jpg"
+  },
+  {
+    "revision": "845a4184adfb27d9625958fe0bbf0c73",
+    "url": "/fresh-avocado/static/media/harim.845a4184.jpg"
+  },
+  {
+    "revision": "0c8f1056fa7b74379c1592058a31427c",
+    "url": "/fresh-avocado/static/media/hilton.0c8f1056.png"
+  },
+  {
+    "revision": "ac846088bc65b6c596760c7bf04822d4",
+    "url": "/fresh-avocado/static/media/ryan.ac846088.png"
+  }
+]);
